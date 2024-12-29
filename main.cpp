3ae92854e2c32e21a4435076c6d77a224faca827
RihebBenRjeb <rihebbenrjeb295@gmail.com>
2024-12-29 20:45:26 +0100

    Add files via upload
    Exercice 2 tp 2

--- main.cpp
+++ main.cpp
@@ -1,38 +1,54 @@
 #include <iostream>
 
 using namespace std;
-
+bool valeur(int x,int y){
+    if(x==y){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool adresse(int *x,int *y){
+    if(*x==*y){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool refe(int &x,int &y){
+    if(x==y){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 int main()
 {
-    int *p,x;
-    float *t,y;
-    //allocation statique avec les pointeurs
-    cout << "donner un entier" << endl;
+    int x,y;
+    cout << "donner x: " << endl;
     cin>>x;
-    p=&x;
-    cout<<"la valeur entiere est : "<<*p<<" et sa valeur d'adresse est : "<<p<<endl;
-    cout << "donner un reel" << endl;
+    cout << "donner y: " << endl;
     cin>>y;
-    t=&y;
-    cout<<"la valeur de ce reel  est : "<<*t<<" et la valeur de son adresse est: "<<t<<endl;
-    //allocation dynamique
-    int *pt=new int(x);
-    cout<<"la valeur entiere est : "<<*pt<<" et sa valeur d'adresse est : "<<pt<<endl;
-    //avec une autre méthode en utilisant les floats
-    float *pf=new float;
-    cout<<"donner la valeur de float : "<<endl;
-    cin>>*pf;
-    cout<<"la valeur de float est : "<<*pf<<" et sa valeur d'adresse est : "<<pf<<endl;
-    //pour libérer la mémoire on utilise:
-    delete pt,pf;
-    //pour vérifier :
-    cout<<"la valeur de float est : "<<*pf<<" et sa valeur d'adresse est : "<<pf<<endl;
-    //mais cela affiche encore les valeurs donc on veut :
-    delete pt;
-    pt = nullptr; // Prévenir un double delete ou un accès invalide
-    delete pf;
-    pf = nullptr;
-    //vérifier
-    cout<<"la valeur de float est : "<<*pf<<" et sa valeur d'adresse est : "<<pf<<endl;
+    if(valeur(x,y)){
+        cout<<x<<" et "<<y<<" sont egaux"<<endl;
+    }
+    else{
+        cout<<x<<" et "<<y<<" ne sont pas egaux"<<endl;
+    }
+     if(adresse(&x,&y)){
+        cout<<x<<" et "<<y<<" sont egaux"<<endl;
+    }
+    else{
+        cout<<x<<" et "<<y<<" ne sont pas egaux"<<endl;
+    }
+     if(refe(x,y)){
+        cout<<x<<" et "<<y<<" sont egaux"<<endl;
+    }
+    else{
+        cout<<x<<" et "<<y<<" ne sont pas egaux"<<endl;
+    }
     return 0;
 }
